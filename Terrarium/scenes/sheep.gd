58873c8f8cb7c929fb54963c565e37643af10d1a
Aidan Dowling <6438565+aidan959@users.noreply.github.com>
2024-04-23 14:20:10 +0100

    addded dd3d, steeringbehaviour interface and template roam behaviour

--- Terrarium/scenes/sheep.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody3D
-
-# - 0 = not hungry, 1 = hungry
-@export_range(0.0,1.0) var hunger : float = 0.0
-
-# - how hungry the sheep gets per frame ? (maybe doing some form of animal_tick later on
-@export_range(0.0,1.0) var metabolism : float = 0.0
-
