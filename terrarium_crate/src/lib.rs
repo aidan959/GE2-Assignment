792ac198ad0ac822810ac4e88d4731ce8efe7e01
Aidan Dowling <6438565+aidan959@users.noreply.github.com>
2024-04-26 16:33:37 +0100

    added some grass and sheep details aswell as rust bindings (maybe?)

--- terrarium_crate/src/lib.rs
+++ terrarium_crate/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use godot::prelude::*;
+mod player;
+use crate::player::Player;
+struct MyExtension;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
